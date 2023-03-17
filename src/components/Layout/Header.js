d818016ef0c1e932a0ea2dad048cc2a5052e5679
kalempic <vodaukolenima5@gmail.com>
2023-03-17 17:33:08 +0100

    first commit

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,15 @@
+import { Fragment } from "react";
+
+const Header=props=>{
+    return <Fragment>
+        <header>
+            <h1>ReactMeals</h1>
+            <button>Cart</button>
+            <div>
+                <img/>
+            </div>
+        </header>
+    </Fragment>
+};
+
+export default Header;
