ac37680fee30cdbc558bfae819db28e71f42eef2
kalempic <vodaukolenima5@gmail.com>
2023-03-20 14:18:35 +0100

    work in progress

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import { Fragment } from "react";
+import { useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
 
 function App() {
+  const[cartIsShown, setChartIsShown]=useState(false);
+
+  const showCartHandler=()=>{ setChartIsShown(true);};
+  const hideCartHandler=()=>{setChartIsShown(false);};
+
   return (
-    <Fragment>
-      <Header/>
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler}/>}
+      <Header onShowCart={showCartHandler}/>
       <main>
         <Meals/>
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
